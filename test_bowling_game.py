8dd5d01c109132395576e41fcf272744fefc1e5b
Heather You <heather.fswd@gmail.com>
2018-11-23 15:15:20 -0500

    Refactor: Add setUp method in test to instantiate Game class for each test

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -2,14 +2,15 @@ import unittest
 from bowling_game import *
 
 class TestBowlingGame(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+
     def test_gutterGame(self):
-        game = Game()
         for i in range(20):
-            game.roll(0)
-        self.assertEqual(game.score(), 0)
+            self.game.roll(0)
+        self.assertEqual(self.game.score(), 0)
     
     def test_allOnes(self):
-        game = Game()
         for i in range(20):
-            game.roll(1)
-        self.assertEqual(game.score(), 20)
+            self.game.roll(1)
+        self.assertEqual(self.game.score(), 20)
