0f456c5a9034be647a54361efb6c74ea84dd927b
Heather You <heather.fswd@gmail.com>
2018-11-23 15:03:09 -0500

    Red: Add test for all 1s rolled per frame, score == 20

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -7,3 +7,9 @@ class TestBowlingGame(unittest.TestCase):
         for i in range(20):
             game.roll(0)
         self.assertEqual(game.score(), 0)
+    
+    def test_allOnes(self):
+        game = Game()
+        for i in range(20):
+            game.roll(1)
+        self.assertEqual(game.score(), 20)
