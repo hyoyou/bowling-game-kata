c59a757ff42c982328586d3c27def4241269aebb
Heather You <heather.fswd@gmail.com>
2018-11-23 14:59:25 -0500

    Red: Add test for gutter game, score == 0

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -4,3 +4,9 @@ from bowling_game import *
 class TestBowlingGame(unittest.TestCase): 
     def test_gameClass(self):
         self.assertTrue(isinstance(Game(), Game))
+
+    def test_gutterGame(self):
+        game = Game()
+        for i in range(20):
+            game.roll(0)
+        self.assertEqual(game.score(), 0)
